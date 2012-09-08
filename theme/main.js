c5b98c23c3e84e7905e22f32b7cd1563a5bebdf2
ScarWu <xneriscool@gmail.com>
2012-09-08 15:59:43 +0800

    2012-09-08 15:59:43

--- theme/main.js
+++ theme/main.js
@@ -23,69 +23,39 @@ $(window).keydown(function(e) {
 				$('.bar .old a')[0].click();
 			break;
 		case 74:
-			scrollBy(0,40);
+			scrollBy(0, 40);
 			break;
 		case 75:
-			scrollBy(0,-40);
+			scrollBy(0, -40);
 			break;
 	}
 });
 
 $('#show').click(function() {
-	var time = 1000;
 	if($('#slider').attr('class') == 'hide') {
-		$('.arrow-right').animate({
-			'left': '+=250'
-		}, time, function() {
-			$('.arrow-left').fadeIn(time/2);
-			$('.arrow-right').fadeOut(time/2, function() {
-				$('.arrow-right').css('left', '0px')
-			});
-		});
-		$('body').animate({
-			'padding-left': '+=250'
-		}, time);
-		$('#main').animate({
-			'margin-left': '-=125'
-		}, time, function() {
-			$('#main').css('margin', '0 auto');
-		});
-		$('#nav').animate({
-			'left': '+=250'
-		}, time);
+		$('body').animate({ 'padding-left': '+=250px' });
+		$('#nav').animate({ 'left': '+=250px' });
 		$('#slider').animate({
-			'left': '+=250'
-		}, time, function() {
+			'left': '+=250px'
+		}, function() {
 			$('#slider').removeClass('hide');
+			$('.arrow-left').fadeIn();
+			$('.arrow-right').fadeOut();
 		});
 	}
 });
 
 $('#hide').click(function() {
-	var time = 1000;
 	if($('#slider').attr('class') != 'hide') {
-		$('.arrow-left').animate({
-			'left': '-=250'
-		}, time, function() {
-			$('.arrow-right').fadeIn(time/2);
-			$('.arrow-left').hide().css('left', '210px');
-		});
-		$('body').animate({
-			'padding-left': '-=250'
-		}, time);
-		$('#main').animate({
-			'margin-left': '+=125'
-		}, time, function() {
-			$('#main').css('margin', '0 auto');
-		});
-		$('#nav').animate({
-			'left': '-=250'
-		}, time);
+		$('body').animate({ 'padding-left': '-=250px' });
+		$('#nav').animate({ 'left': '-=250px' });
 		$('#slider').animate({
-			'left': '-=250'
-		}, time, function() {
+			'left': '-=250px'
+		}, function() {
 			$('#slider').addClass('hide');
-	});
+			$('.arrow-right').fadeIn();
+			$('.arrow-left').fadeOut();
+		});
 	}
 });
 
